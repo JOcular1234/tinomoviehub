5666e03cba21038e66edd14414a395df520c6e44
Mfon-obong Umoh <mfonobongumoh75@gmail.com>
2025-08-02 05:17:51 +0100

    updates made

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,3 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// export default defineConfig({
-//   plugins: [react()],
-//   build: {
-//     // outDir: 'dist',
-//     // assetsDir: 'assets',
-//     // emptyOutDir: true,
-//   },
-//   root: '.', // adjust if your source is elsewhere
-// })
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -22,9 +9,4 @@ export default defineConfig({
     // emptyOutDir: true,
   },
   root: '.', // adjust if your source is elsewhere
-  server: {
-    proxy: {
-      '/api': 'http://localhost:5000', // Proxy API requests to backend
-    },
-  },
 })
